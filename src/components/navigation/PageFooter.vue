95be7fca657863156614642e7cc83864e6a71bba
David Ibia <david.ibia@boxmarshall.com>
2021-06-08 22:40:07 +0100

    feat: created basic footer

---
+++ src/components/navigation/PageFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer>
+    <span class="copyright">
+      &copy; {{ year }} David Ibia. All Rights Reserved.
+    </span>
+  </footer>
+</template>
+
+<script>
+  export default {
+    name: 'page-footer',
+    computed: {
+      year() {
+        return new Date().getFullYear()
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    background-color: #2d232e;
+    color: #fff;
+    height: 100px;
+    display: grid;
+    place-items: center center;
+  }
+</style>
