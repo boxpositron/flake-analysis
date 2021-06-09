febb596b100945d9583d029ce0fa62096b3769b0
David Ibia <david.ibia@boxmarshall.com>
2021-06-09 02:12:51 +0100

    feat: added navbar feat: added routes

--- src/components/navigation/Navbar.vue
+++ src/components/navigation/Navbar.vue
@@ -1,5 +1,15 @@
 <template>
-  <div></div>
+  <nav>
+    <div class="brand">
+      <div class="brand-text">
+        Flake Analysis
+      </div>
+    </div>
+    <div class="links">
+      <router-link to="/info">Information</router-link>
+      <router-link to="/delta">Compare</router-link>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -8,4 +18,48 @@
   }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  nav {
+    background-color: #8980f5;
+
+    display: grid;
+    grid-template-areas: '. brand . links .';
+    grid-template-columns:
+      minmax(auto, 200px)
+      max-content
+      auto
+      max-content
+      minmax(auto, 200px);
+    grid-auto-rows: auto;
+    font-family: 'Source Code Pro', monospace;
+
+    div.brand {
+      grid-area: brand;
+
+      div.brand-text {
+        color: #fff;
+        font-weight: 600;
+        line-height: 50px;
+      }
+    }
+
+    div.links {
+      grid-area: links;
+
+      a {
+        display: inline-block;
+        color: #fff;
+        text-decoration: none;
+        font-weight: 600;
+        padding: 0 20px;
+        margin-left: 20px;
+        height: 50px;
+        line-height: 50px;
+
+        &.router-link-active {
+          color: #2d232e;
+        }
+      }
+    }
+  }
+</style>
